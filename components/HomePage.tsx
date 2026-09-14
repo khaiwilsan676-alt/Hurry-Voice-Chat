9d13d00c6d579d1cd6256de5d048db5e9911d627
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-09-14 10:31:03 +0530

    Fix formatting in HomePage component

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1906,7 +1906,7 @@ export default function HomePage({ onLogout }: HomePageProps) {
         )}
       </div>
 
-      <div className="flex gap-2 mb-3"
+      <div className="flex gap-2 mb-3">
         <button
           type="button"
           onClick={() => setActiveMineTab('following')}
