0cfeba425ec364e50d4a649e6680777d1c31a626
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-09-15 14:30:39 +0530

    Adjust layout and image properties in MePage component

--- components/MePage.tsx
+++ components/MePage.tsx
@@ -910,7 +910,7 @@ export default function MePage({ onLogout, onPublicProfileChange }: MePageProps)
           </div>
         </div>
 
-        <div className="flex gap-0.5 mt-4">
+        <div className="flex gap-1 mt-4">
           <div 
             className="flex-1 rounded-lg overflow-hidden cursor-pointer active:scale-95 transition-transform"
             onClick={() => {
@@ -942,11 +942,11 @@ export default function MePage({ onLogout, onPublicProfileChange }: MePageProps)
               
 
         {/* 👇 BASS YAHAN YEH NAYA CODE ADD KARNA HAI 👇 */}
-        <div className="-mt-7 rounded-lg overflow-hidden w-full">
+        <div className="-mt-11 rounded-lg overflow-hidden w-full">
           <img
             src="/file_00000000158082118e896234213c6673.png"
             alt="SVIP"
-            className="w-full h-auto object-cover shadow-sm"
+            className="w-95 h-auto object-cover shadow-sm"
           />
         </div>
         {/* 👆 NAYA CODE YAHAN KHATAM 👆 */}
