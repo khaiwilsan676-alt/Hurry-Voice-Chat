eaa7405b82ffde13cc927de45cd92bc351c00e85
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-09-26 16:21:34 +0300

    Fix WebView lambda compile error in Android MainActivity

--- android/app/src/main/java/com/hawa/app/MainActivity.java
+++ android/app/src/main/java/com/hawa/app/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends BridgeActivity {
                         WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars()
                     );
                     view.setPadding(bars.left, 0, bars.right, bars.bottom);
-                    view.post(() -> view.evaluateJavascript(
+                    view.post(() -> webView.evaluateJavascript(
                         "document.documentElement.style.setProperty('--status-bar-height','" + bars.top + "px')",
                         null
                     ));
